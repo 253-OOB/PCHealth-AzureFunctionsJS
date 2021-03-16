e81522922671acaf295bccd82d31157f1c1d297f
florardu <andretabourian@hotmail.com>
2021-03-16 13:20:56 +0200

    Added mock values script for the database

---
+++ res/SQL/Login_Tables_Data.sql
@@ -0,0 +1,5 @@
+
+-- INSERT INTO [PC_Health].[dbo].[Organisations] (Name, Password, Email) VALUES ('aub', '123', 'it@aub.com')
+
+-- INSERT INTO [PC_Health].[dbo].[Accounts] (Email, OrganisationID, Username, Password) 
+--     VALUES ('aat36@mail.aub.edu', 1, 'aat36', 'abc123')
