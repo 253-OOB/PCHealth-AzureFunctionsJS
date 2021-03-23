27ea2c9f28479c5de0d447dc00d205accad9f619
florardu <andretabourian@hotmail.com>
2021-03-23 20:06:06 +0200

    WebAppService home page ready to be deployed

--- WebappService/home/index.js
+++ WebappService/home/index.js
@@ -1,13 +1,15 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = async function (context, req) {    
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const spliturl = req.url.split("/");
+    const filetype = spliturl[ spliturl.length - 1 ].split(".")[1];
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        status: 200,
+        headers: {
+            "Content-Type": "text/" + filetype
+        },
+        body : context.bindings.indexBlob,
+        isRaw: true
     };
+        
 }
