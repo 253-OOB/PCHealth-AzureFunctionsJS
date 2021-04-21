df2a09c128ba792128855fbee481fb8c41fa93a3
florardu <andretabourian@hotmail.com>
2021-04-22 01:08:48 +0300

    Added an assign endpoint

--- WebappService/tags/index.js
+++ WebappService/tags/index.js
@@ -5,16 +5,92 @@ const jwt = require("../helpers/jwtVerify");
 
 module.exports = async function (context, req) {
     
-    console.log("Hello");
-
     if( req.params.operation == "create" ) {
         context.res = await createTag(req);
     } else if( req.params.operation == "get" ) {
         context.res = await getTags(req);
     } else if ( req.params.operation == "delete" ) {
         context.res = await deleteTag(req);
-    } else {
+    } else if ( req.params.operation == "assign" ) {
+        context.res = await assignTag(req);
+    }   else {
         context.res = {status: 404};
+    }
+
+}
+
+async function assignTag ( req ) {
+
+    // ?TagID=x&LeafID=x
+
+    let auth = await verifyAccountJwtIdentity( req );
+
+    if( auth.status !== 200 ) {
+        return {status: auth.status};
+    }
+
+    let Email = auth.payload.payload;
+
+    if( !("TagID" in req.query && "LeafID" in req.query) ) {
+        return {status: 400};
+    }
+
+    let inputs = [
+
+        {name: "AccountEmail", type: sql.NVarChar, value: Email},
+        {name: "TagID", type: sql.Int, value: parseInt(req.query.TagID)},
+        {name: "LeafID", type: sql.Int, value: parseInt(req.query.LeafID)}
+
+    ]
+
+    let sqlQueryString = `
+        SELECT AccountID FROM proj09.Accounts_Organisations_V as t1 
+            inner join proj09.Leaf as t2 on t1.OrganisationID = t2.OrganisationID  
+                WHERE t1.AccountEmail=@AccountEmail AND t2.LeafID=@LeafID
+    `;
+    let sqlResult = await sql.query(sqlQueryString, inputs);
+
+    if (sqlResult.status !== 200) {
+        return {status: sqlResult.status};
+    }
+
+    if (sqlResult.data.length !== 1) {
+        return { status: 403 };
+    }
+
+    try {
+
+        sqlQueryString = "INSERT INTO proj09.LeafTags(LeafID, TagID) VALUES (@LeafID, @TagID)"
+        sqlResult = await sql.query(sqlQueryString, inputs);
+    
+    } catch (err) {
+
+        return {status: 500};
+        
+    }
+
+    if (sqlResult.status !== 200) {
+        return {status: sqlResult.status};
+    }
+
+    if( "accessToken" in auth ) {
+        
+        return {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+                AccessToken: auth.accessToken
+            }
+        };
+    
+    } else {
+
+        return {
+            status: 200
+        };
+
     }
 
 }
@@ -22,15 +98,13 @@ module.exports = async function (context, req) {
 async function createTag ( req ) {
 
     // ?Tag=x
-    let auth = verifyJwtIdentity( req );
+    let auth = verifyOrganisationJwtIdentity( req );
 
     if( auth.status !== 200 ) {
         return {status: auth.status};
     }
 
     let OrganisationEmail = auth.payload;
-
-    console.log(auth);
 
     let tagToInsert = null;
 
@@ -51,8 +125,6 @@ async function createTag ( req ) {
     if (sqlResult.status !== 200) {
         return {status: sqlResult.status};
     }
-
-    console.log(sqlResult);
 
     if( sqlResult.data.length !== 1 ) {
         return {status: 500};
@@ -77,7 +149,7 @@ async function getTags( req ) {
         return {status: 400};
     }
 
-    let inputs = [{name: "OrganisationID", type: sql.Int, value: req.query.OrganisationID}]; 
+    let inputs = [{name: "OrganisationID", type: sql.Int, value: parseInt(req.query.OrganisationID)}]; 
     sqlQueryString = "SELECT * FROM proj09.Tags WHERE OrganisationID=@OrganisationID";
 
     let sqlResult = await sql.query(sqlQueryString, inputs);
@@ -102,7 +174,7 @@ async function deleteTag( req ) {
 
     // ?Tag=x
 
-    let auth = verifyJwtIdentity( req );
+    let auth = verifyOrganisationJwtIdentity( req );
 
     if( auth.status !== 200 ) {
         return {status: auth.status};
@@ -135,7 +207,7 @@ async function deleteTag( req ) {
 
 }
 
-function verifyJwtIdentity( req ) {
+function verifyOrganisationJwtIdentity( req ) {
 
     // GET JWT IDENTITY
 
@@ -150,5 +222,27 @@ function verifyJwtIdentity( req ) {
     let auth = jwt.verifyAccessToken(organisationToken);
 
     return auth;
+
+}
+
+async function verifyAccountJwtIdentity( req ) {
+
+    // GET JWT IDENTITY
+
+    let refreshToken = null;
+    let accessToken = null;
+
+    if ( "AccessToken" in req.body ) {
+        accessToken = req.body.AccessToken;
+    }
+
+    if ( "RefreshToken" in req.body ) {
+        refreshToken = req.body.RefreshToken;
+    }
+
+    let auth = await jwt.evaluateAuthentication(accessToken, refreshToken)
+
+    return auth;
+
 
 }
